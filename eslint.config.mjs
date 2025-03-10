312e51e89ce01652f5930ff54fd12f3e78dcf7f1
Jairo Miranda <jairo.miranda@sescgo.com.br>
2025-03-10 13:58:34 -0300

    Correção do prettierrc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -30,7 +30,7 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      'linebreak-style': ['error', 'windows'],
+      "prettier/prettier": ["error", { "endOfLine": "auto" }]
     },
   },
 );
