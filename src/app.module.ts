2d75f80a707cc4525a91fcf94ec60a6524b4e5a6
Jairo Miranda <jairo.miranda@sescgo.com.br>
2025-03-11 11:42:24 -0300

    Conclusão da remoção da controller App

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AtividadeController } from './atividade/atividade.controller';
-import { UserService } from './user/user.service';
-import { ProdutoService } from './produto/produto.service';
 import { ProdutoController } from './produto/produto.controller';
+import { ProdutoService } from './produto/produto.service';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, AtividadeController, ProdutoController],
-  providers: [AppService, UserService, ProdutoService],
+  controllers: [AtividadeController, ProdutoController],
+  providers: [UserService, ProdutoService],
 })
 export class AppModule {}
